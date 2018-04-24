1951fadeee5a4bcc6b298007d35ab12f96d7e603
Vishal Kuo <vishalkuo@hotmail.com>
2018-04-24 09:32:48 -0700

    Parsing

--- analyze.py
+++ analyze.py
@@ -1,7 +1,7 @@
 from enum import Enum
 from typing import List
 
-class ChargeTypes(Enum):
+class ChargeType(Enum):
     PAYMENT = 1
     STANDARD_TRANSFER = 2
     CHARGE = 3
@@ -11,7 +11,7 @@ class ChargeStatus(Enum):
     ISSUED = 2
     PENDING = 3
 
-class HeaderRow(Enum):
+class Header(Enum):
     ID = 0
     DATE_TIME = 1
     TYPE = 2
@@ -31,10 +31,25 @@ def _load_and_preprocess(csv_file: str) -> List[str]:
     with open(csv_file) as f:
         data = f.read()
 
-    data = data.split("\n")[1:]
+    data = data.split("\n")[1:-1]
     data = [d.split(",") for d in data]
     for i in range(len(data)):
         data_row = data[i]
+        to_enum(data_row, Header.TYPE, ChargeType)
+        to_enum(data_row, Header.STATUS, ChargeStatus)
+        data_row[Header.AMT] = parse_dollar(data_row[Header.AMT.value])
+        data_row[Header.FEE] = parse_dollar(data_row[Header.FEE.value])
+
+
+def to_enum(row: List[str], header: Header, klass) -> None:  
+    item = row[header.value]
+    item = item.upper()
+    item = item.replace(" ", "_")
+    row[header.value] = klass[item]
+
+def parse_dollar(dlr_str: str) -> float:
+    pass
+
 
 if __name__ == "__main__":
-    process_csv("Vishal Kuo", "download (1).csv")
+    process_csv("Vishal Kuo", "samples/download (1).csv")
